bf093b5d1dc3ac45bdc72fe9ca1f045ed0f3ebe8
MohamedAmhal <mohammedamhal349@gmail.com>
2024-06-27 16:44:18 -0700

    adding the second task

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from './0-classroom.js'
+
+function initializeRooms() {
+  return [
+    ClassRoom(19),
+    ClassRoom(20),
+    ClassRoom(34)
+  ];
+}
+
+export default initializeRooms;
