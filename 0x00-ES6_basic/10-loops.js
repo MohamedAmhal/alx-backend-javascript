d6f6c8aa2f6f767105e0716878b77af0c936a5de
MohamedAmhal <mohammedamhal349@gmail.com>
2024-06-28 10:30:05 -0700

    up

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    const value = array.indexOf(idx);
-    array[value] = appendString + idx; // asign the value
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    array[idx] = appendString + value; // eslint-disable-line no-param-reassign
   }
 
   return array;
