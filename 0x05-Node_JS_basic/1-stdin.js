838a6c7ba3fd3710cbb20709ecb93925d33aca92
MohamedAmhal <mohammedamhal349@gmail.com>
2024-08-20 19:42:41 +0100

    updating

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+/**
+ * Logs a message .
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) =>{
+    process.stdout.write(`Your name is: ${data}`);
+});
+
+process.stdin.end('end', () =>{
+    process.stdout.write('This important software is now closing\n');
+});
