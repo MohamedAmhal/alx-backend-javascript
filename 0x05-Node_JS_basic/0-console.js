848587b5f97dddde20f20b6db55214c395a06653
MohamedAmhal <mohammedamhal349@gmail.com>
2024-08-20 18:29:06 +0100

    updating

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,14 @@
-function displayMessage(message){
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message.
+ * @returns {void}
+ */
+const displayMessage = (message) =>{
     console.log(message);
 }
+/**
+ * Exports.
+ * @module displayMessage
+ * @function
+ */
 module.export = displayMessage;
